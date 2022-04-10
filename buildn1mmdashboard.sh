c458fc101dfd9230db6773191f7642ede3a652a5
mboroff <martin.boroff@gmail.com>
2022-04-10 18:41:12 -0400

    Update buildn1mmdashboard.sh

--- buildn1mmdashboard.sh
+++ buildn1mmdashboard.sh
@@ -38,7 +38,7 @@ echo "Installing upgrade"
 sudo apt update && sudo apt full-upgrade -y && sudo apt clean
 clear
 echo " "
-
+sudo apt install build-essential git curl
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) < /home/pi/testbuildn1mmdashboard/y.txt
 sleep 2
 
